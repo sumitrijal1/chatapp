c92aade63780e985d543ee843f511dbbd854303c
sumitrijal1 <rijalsumit50@gmail.com>
2026-06-27 23:43:01 +0545

    handlin socket properly

--- backend/controller/global/globalcontroller.js
+++ backend/controller/global/globalcontroller.js
@@ -1,15 +1,19 @@
  import db from '../../db.js'
 import { executeWithRetry } from '../../services/dbretry.js'
+import { getIo } from '../../services/socket.js'
 
 export const getallusers = async(req,res)=>{
     const userid= req.user.id
+    console.log("logged in userid:", userid) 
     const [users] = await executeWithRetry(db, 'SELECT id,name,email FROM users WHERE id != ?',[userid])
-     
+     console.log("users returned:", users)
+    
     if(users.length >0){
         res.status(200).json({
             message:'users retrieved successfully',
             data:users
         })
     }
+
 }
 
