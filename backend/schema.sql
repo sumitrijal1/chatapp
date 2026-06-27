c02912f5908dbb6df8cb590c643718240d40488f
sumitrijal1 <rijalsumit50@gmail.com>
2026-06-27 23:42:08 +0545

    image column change

--- backend/schema.sql
+++ backend/schema.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS users(
     name     VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    image    VARCHAR(255) NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
